e0febac0c8c9c827a005e7a881f14d57aeab02b9
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-22 20:44:07 +0200

    Add files via upload

---
+++ Zadacha 13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Zadachi_Nakov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"(.+):\/\/(www\..+\.\w+)(\/.+\..+)");
+            string input = Console.ReadLine();
+            Match match = reg.Match(input);
+            Console.WriteLine("[protocol] = " + match.Groups[1].ToString());
+            Console.WriteLine("[server] = " + match.Groups[2].ToString());
+            Console.WriteLine("[resource] = " + match.Groups[3].ToString());
+
+
+        }
+    }
+}

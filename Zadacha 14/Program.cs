a000aa3faf7dedb783bddae9d8481e2ecba25621
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-22 20:58:01 +0200

    Add files via upload

---
+++ Zadacha 14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ").ToArray();
+
+            for (int i = input.Length; i > 0; i--)
+            {
+                Console.Write(input[i-1] + " ");
+            }
+        }
+    }
+}

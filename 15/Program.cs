6b1cbbed369f43aec12c72f5027d9c5de965e30e
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-23 01:38:37 +0200

    Add files via upload

---
+++ 15/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"<a href=(.+>.+)<\/a>");
+
+            string input = Console.ReadLine();
+
+            input.Replace(reg.Match(input).ToString(), "[URL=…]" + reg.Match(input).Groups[1].ToString() + "/URL]");
+            MatchCollection mc = reg.Matches(input);
+            
+            Console.WriteLine(input);
+            Console.WriteLine(mc.Count);
+        }
+    }
+}

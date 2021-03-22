56945dcb78d56f61245ea788da6d338f70ef09f3
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-23 01:47:22 +0200

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ").ToArray();
+            StringBuilder a = new StringBuilder();
+            StringBuilder b = new StringBuilder();
+
+            foreach (var item in input)
+            {
+
+              
+
+                if(item.Length%2 != 0)
+                    {
+
+                    for (int i = 0; i < item.Length / 2; i++)
+                    {
+                        a.Append(item[i].ToString());
+                    }
+                    for (int i = item.Length - 1; i > item.Length / 2; i--)
+                    {
+                        b.Append(item[i].ToString());
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < item.Length / 2; i++)
+                    {
+                        a.Append(item[i].ToString());
+                    }
+                    for (int i = item.Length - 1; i > item.Length / 2 - 1; i--)
+                    {
+                        b.Append(item[i].ToString());
+                    }
+                }
+
+                if (a.ToString() == b.ToString())
+                {
+                    Console.Write(item + " ");
+                }
+                a.Clear();
+                b.Clear();
+            }
+        }
+    }
+}

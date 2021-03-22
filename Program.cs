cc029600159556d31ed7eb2c12cc0a278b9ffe64
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-22 19:25:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Zadacha10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternWord = Console.ReadLine();
+            string[] usernames = Console.ReadLine().Split(". ");
+            Console.WriteLine(string.Join(Environment.NewLine, usernames
+                .Where(x => Regex.IsMatch(x, @"(\b)" + patternWord + @"(\b)?"))));
+        }
+    }
+}

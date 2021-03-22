ed90790fb8baa676c8b66dbdb2cc7f688334ef80
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-22 19:23:23 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool bracket = true;
+
+            foreach (var item in input)
+            {
+                if (item == '(')
+                {
+                    bracket = false;
+                }
+                if (bracket == false)
+                {
+                    if (item == ')')
+                    {
+                        bracket = true;
+                    }
+                }
+                if (item == ')' && bracket == true)
+                {
+                    Console.WriteLine("no");
+                    return;
+                }
+            }
+            if (bracket == false)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine("yes");
+            }
+        }
+    }
+}

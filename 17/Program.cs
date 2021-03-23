d3f616d860dbe70714f8918111a3c288ca3a13b1
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-23 02:08:52 +0200

    Add files via upload

---
+++ 17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"/^\S+@\S+\.\S+$/");
+            string input = Console.ReadLine();
+            MatchCollection mc = reg.Matches(input);
+
+            foreach (var item in mc)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}

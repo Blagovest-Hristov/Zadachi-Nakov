697264ddf7af428cabf2a913a52ee82fa042ae25
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-24 09:49:33 +0200

    Add files via upload

---
+++ 22/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"\w");
+            string input = Console.ReadLine();
+            string result ="";
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            foreach (var item in reg.Matches(input))
+            {
+                result += item.ToString();
+                if (dic.ContainsKey(item.ToString()))
+                {
+                    dic[item.ToString()]++;
+                }
+                else
+                {
+                    dic.Add(item.ToString(), 1);
+                }
+            }
+            char[] a = result.ToCharArray();
+            Array.Sort(a);
+            result = new string(a);
+            Console.WriteLine(result);
+            foreach (var item in dic)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+        }
+    }
+}
+

513fa92074bb7a1e8a143bf937cafda98ce05b5e
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-29 23:26:28 +0300

    Add files via upload

---
+++ 25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = Console.ReadLine().Split(", ").ToList();
+            stringList.Sort();
+
+            foreach (var item in stringList)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}

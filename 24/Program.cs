10f02458df19d62eba39347ba46fae23fb76d4ad
Blagovest-Hristov <72490709+Blagovest-Hristov@users.noreply.github.com>
2021-03-29 23:08:55 +0300

    Add files via upload

---
+++ 24/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"(?<number>.)\k<number>+");
+            string input = Console.ReadLine();
+            string constant = input;
+
+            for (int i = 0; i < reg.Matches(constant).Count ; i++)
+            {
+                
+                Console.Write(reg.Match(input).Groups[1]);
+                input = input.Replace(reg.Match(input).ToString(), "");
+            }
+            
+        }
+    }
+}
